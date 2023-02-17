8f2939ee6142976c11ad6967314de2d02ce980a0
Jordyn <jordyn@husk.pro>
2023-02-17 17:54:21 +1100

    swap <leader>ff to git files, <leader>af to all files. fix pane mapping

--- lua/husk/remap.lua
+++ lua/husk/remap.lua
@@ -1,6 +1,6 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("n", "<C-h>", vim.cmd.wincmd({ args = { "h" } }))
-vim.keymap.set("n", "<C-j>", vim.cmd.wincmd({ args = { "j" } }))
-vim.keymap.set("n", "<C-k>", vim.cmd.wincmd({ args = { "k" } }))
-vim.keymap.set("n", "<C-l>", vim.cmd.wincmd({ args = { "l" } }))
+vim.keymap.set("n", "<C-h>", function () vim.cmd.wincmd({ args = { "h" } }) end)
+vim.keymap.set("n", "<C-j>", function () vim.cmd.wincmd({ args = { "j" } }) end)
+vim.keymap.set("n", "<C-k>", function () vim.cmd.wincmd({ args = { "k" } }) end)
+vim.keymap.set("n", "<C-l>", function () vim.cmd.wincmd({ args = { "l" } }) end)
