e6aaa31f3632fed072b721b09c5a8aed36383e98
Jordyn <jordyn@husk.pro>
2023-05-21 09:59:30 +1000

    feat: add codieum icon

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -63,6 +63,7 @@ cmp.setup({
             mode = 'symbol', -- show only symbol annotations
             maxwidth = 50,   -- prevent the popup from showing more than provided characters
             ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
+            symbol_map = { Codeium = "", }
         })
     }
 })
